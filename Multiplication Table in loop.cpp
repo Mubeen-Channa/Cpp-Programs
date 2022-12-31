2793e5b67039f1792c4d6de1186ec5b287dbc7a2
Muhammad Mubeen <116976135+Mubeen-Channa@users.noreply.github.com>
2022-12-31 19:11:00 +0500

    Generate Multiplication Table Of A Number Entered By User Using For oop.

---
+++ Multiplication Table in loop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ using namespace std;
+  int main( )
+  // github.com/Mubeen-Channa
+{
+	int num;
+	cout<<"Enter a number you want to print table : "; cin>>num;
+	
+	for( int z=1; z<=10; z++ )
+	{
+		cout<<z<<" * "<<num<<" = "<<z*num<<endl;
+	}
+	
+  return 0;
+}
+
+
+
