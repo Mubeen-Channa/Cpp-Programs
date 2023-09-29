b5e8c245658e4bba3bfdd708b3d5a763e9322cee
Muhammad Mubeen Channa <116976135+Mubeen-Channa@users.noreply.github.com>
2023-09-30 02:38:42 +0500

    Find The Address Of Variable
    This is a Find The Address Of Variable program in C++, in which the user can easily find the address of variable.

---
+++ Find The Address Of Variable.cpp
@@ -0,0 +1,20 @@
+// We Can find the address of any variable by using ' & ' operator
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+   int  first, sec;
+   
+   cout << "Enter The Value Of First and Second Variable \n\n";
+   cin >> first >> sec;
+
+   cout << "Address Of First Variable :\n\n";
+   cout << &first <<"\n";
+
+   cout << "\nAddress Of Second Variable :\n\n";
+   cout << &sec << endl;
+
+   return 0;
+}
