660630359bb57ac074747371795e32699e35ea2e
Muhammad Mubeen Channa <116976135+Mubeen-Channa@users.noreply.github.com>
2023-09-30 02:36:21 +0500

    Display Size Of Different Datatype
    This is a Display Size Of Different Datatype program in C++, in which you can find the size of datatype.

---
+++ Display Size Of Different Datatype.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ cout<<"size of int "<<sizeof(int)<<endl;
+ cout<<"size of float "<<sizeof(float)<<endl;
+ cout<<"size of char "<<sizeof(char)<<endl;
+ cout<<"size of double "<<sizeof(double)<<endl;
+ cout<<"size of string "<<sizeof(string)<<endl;
+ cout<<"size of long "<<sizeof(long)<<endl;
+ cout<<"size of long double "<<sizeof(long double)<<endl;
+
+ return 0;
+}
