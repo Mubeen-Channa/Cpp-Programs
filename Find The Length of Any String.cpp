bfc0dced8b95584cb14c5138ce12664ce49eecd1
Muhammad Mubeen Channa <116976135+Mubeen-Channa@users.noreply.github.com>
2023-10-05 23:42:41 +0500

    Find The Length of Any String.cpp

--- Find The Length of Any String.cpp
+++ Find The Length of Any String.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
 	string str;
-	cout << "Enter An Any Single String : "; cin>>str;
+	cout << "Enter An Any Single String : "; getline(cin, str);
 
     int count=0;
 	for (int i=0; i<str.length(); i++) {
