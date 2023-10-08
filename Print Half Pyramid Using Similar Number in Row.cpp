4163f66a076c6fd47917962c3f2e5f6c6846e177
Muhammad Mubeen Channa <116976135+Mubeen-Channa@users.noreply.github.com>
2023-10-08 16:17:30 +0500

    Print Half Pyramid Using Similar Number in Row
    This is a Print Half Pyramid Using Similar Number in Row program in C++, in which the user enters a range of rows then the program Print Half Pyramid Using Similar Number in Row.

---
+++ Print Half Pyramid Using Similar Number in Row.cpp
@@ -0,0 +1,24 @@
+// 1
+// 2 2 
+// 3 3 3 
+// 4 4 4 4 
+// 5 5 5 5 5
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter How Many Row Print With Pattern : "; cin >>n;
+
+    for (int i=1; i<=n; i++)
+    {
+        for (int j=1; j<=i; j++)
+        {
+            cout << i <<" ";
+        }
+            cout << "\n";
+    }
+    return 0;
+}
