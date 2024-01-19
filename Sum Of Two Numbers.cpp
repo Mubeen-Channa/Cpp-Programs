c60219a5bb709d95013475ec2fec8265835214c2
Muhammad Mubeen Channa <116976135+Mubeen-Channa@users.noreply.github.com>
2024-01-19 23:08:11 +0500

    Sum Of Two Numbers.cpp

---
+++ Sum Of Two Numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1, num2, sum;
+
+    cout << "Enter 2 Numbers Respectively : ";
+
+    cin >> num1 >> num2;
+
+    sum = num1 + num2;
+
+    cout << "Sum of 2 Numbers are : " << sum;
+    return 0;
+}
