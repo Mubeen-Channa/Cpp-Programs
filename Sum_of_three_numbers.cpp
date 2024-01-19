ada74d52fac81595d997ecfb74d36931343ebe27
Muhammad Mubeen Channa <116976135+Mubeen-Channa@users.noreply.github.com>
2024-01-20 00:04:35 +0500

    Sum Of Three Numbers

---
+++ Sum_of_three_numbers.cpp
@@ -0,0 +1,16 @@
+//Sum of Three Numbers
+
+#include <iostream>
+using namespace std;
+int main(){
+    int num1, num2, num3, Sum;
+
+    cout<<"Enter Frist Number: ";       cin>>num1;
+    cout<<"Enter Sencond Number: "; cin>>num2;
+    cout<<"Enter Third Number: ";      cin>>num3;
+
+    Sum = num1 + num2 + num3;
+
+    cout<<"Sum of three numbers are : "<< Sum;
+    return 0;
+}
