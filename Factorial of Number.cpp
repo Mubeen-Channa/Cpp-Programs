30c0e19c206ca2856380aaa040f0cddce0d07b49
Muhammad Mubeen Channa <116976135+Mubeen-Channa@users.noreply.github.com>
2024-01-25 11:50:32 +0500

    Factorial of a Number.cpp

--- Factorial of Number.cpp
+++ Factorial of Number.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
 using namespace std;
+int main(){
+    
+    // User Input
+    int num, Factorial = 1;
+    cout<< "Enter a number to find its factorial: ";    cin >> num;
 
-int main()
-{
-    int num, factorial = 1;
-    cout << "Enter a Number: ";
-    cin >> num;
-
-    if (num < 0)
-    {
-        cout << "Factorial of a negative number is not possible";
+    // Condition to check wheather a number is positive or negative
+    if(num < 0){
+        cout<<"Factorial of negative number is not possible";
     }
-    else
-    {
-        for (int z = 1; z <= num; z++)
-        {
-            factorial = factorial * z;
+    else{
+        for(int i = 1; i <= num; i++){
+            Factorial = Factorial * i;
         }
-        cout << "Factorial of " << num << " is " << factorial;
     }
+
+    cout<<"Factorial of "<< num << " is "<< Factorial;
     return 0;
 }
